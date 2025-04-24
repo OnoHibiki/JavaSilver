da3c81c20b3176ded776d363b697d51b5f68aa89
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-04-24 23:29:40 +0900

    4/24

---
+++ 4月/24日/P242_1.java
@@ -0,0 +1,10 @@
+
+public class P242_1 {
+    
+        int num1 = 0;
+        var num2 = 10; //ダメ　メンバ変数で型推論は使えない。
+        private String num3; 
+        protected String[] array;
+        public static final long value; //ダメ　finalは初期化が必要。
+    
+}
