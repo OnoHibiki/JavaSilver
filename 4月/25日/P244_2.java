ff0ab62df42ace66dc4f000269410ab5bc3206a3
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-04-25 23:04:21 +0900

    4/25

---
+++ 4月/25日/P244_2.java
@@ -0,0 +1,12 @@
+
+public class P244_2 {
+    public static void main(String[] args){
+        P244_2 obj = new P244_2();
+        double v = obj.x(10);
+        System.out.println(v);
+    }
+
+    // insert code here
+    public float x(int a) {return a * 0.5F;}
+
+}
