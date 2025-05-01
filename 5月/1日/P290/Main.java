e26f3bec6fa13a5c79946eec45dbd8da7bde5e46
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-01 19:55:47 +0900

    5/1

---
+++ 5月/1日/P290/Main.java
@@ -0,0 +1,30 @@
+class Item {
+    private int id;
+    Item(int id) {
+        this.id = id;
+    }
+
+    public void display(){
+        System.out.println("Item id : " + id);
+    }
+}
+
+class Clothes extends Item {
+    private String brand;
+    Clothes(int id, String brand) {
+        super(id);
+        this.brand = brand;
+    }
+    @Override
+    public void display(){
+        super.display();
+        System.out.println("Clothes brand : " + brand);
+    }
+}
+
+class Main {
+    public static void main(String[]args){
+        Clothes c = new Clothes(100,"Duke's Clothes");
+        c.display();
+    }    
+}
