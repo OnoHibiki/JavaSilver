559353befa954fec2b4e2eb5a6eda3a4f689463a
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-03 22:58:44 +0900

    5/3

--- 5月/3日/P375/Curriculum.java
+++ 5月/3日/P375/Curriculum.java
@@ -5,7 +5,7 @@ class Main{
     public static void main(String[]args){
         Curriculum obj1 = new Curriculum("English", "intermediate");
         Curriculum obj2 = new Curriculum("Programming", "Advanced");
-        obj1.name = "Japanese"; //レコードクラスのフィールドは全てprivateかつfinalのため、他クラスからのアクセス、値の代入ができない
+        //obj1.name = "Japanese"; //レコードクラスのフィールドは全てprivateかつfinalのため、他クラスからのアクセス、値の代入ができない
         System.out.println(obj1);
         String val = obj2.name() + "," + obj2.level();
         System.out.println("obj2:" + val);
