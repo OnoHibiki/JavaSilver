4e9ad54e14af4915c40bd1531855ae7547f644cf
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-04 14:23:16 +0900

    5/4

--- 5月/4日/P406/Main.java
+++ 5月/4日/P406/Main.java
@@ -6,6 +6,7 @@ public class Main{
                 System.out.println("try - " + args[i]);
             } catch(ArrayIndexOutOfBoundsException e){
                 System.out.println("catch - " + e.getMessage());
+                break;//これがあった方がぽいよね
             }
         }
    }
