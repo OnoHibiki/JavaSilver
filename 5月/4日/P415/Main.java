4fc243497854c246fb01dcdede60e617a00fb0df
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-04 21:24:08 +0900

    5/4

---
+++ 5月/4日/P415/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Main {
+    public static void main(String[]args){
+        FileWriter fw = null;
+        try{
+            fw = new FileWriter("test.txt");
+            fw.write("try-catch-finally");
+        } catch(IOException e){
+            e.printStackTrace();
+        } finally {
+            if(fw != null){
+                try {
+                    fw.close();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
