5579a594c6e19ccfb46892057b25d577b9309308
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-05 10:59:13 +0900

    5/5

---
+++ 5月/5日/P428/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+public class Main {
+    public static void main(String[]args){
+        Main obj = new Main();
+        try{
+            obj.checkedA();
+        }catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        obj.unchecked();
+    }
+    public void checkedA() throws IOException,ClassNotFoundException{
+        checkedB();
+    }
+    public void checkedB() throws ClassNotFoundException{}
+    public void unchecked() throws RuntimeException{}
+}
