411a2b89fb09e5184970298f65a7eda245d51b23
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-12 23:09:38 +0900

    5/12

---
+++ 5月/12日/P182/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[]args){
+        char[] chars = new char[]{'A','P','P','L','E'};
+        int value = 0;
+        for(char c : chars){
+            value += switch(c) {
+                case 'A': yield 1;
+                case 'P' : yield 2 ;
+                default: yield 3;
+            };
+
+            //System.out.println(value);
+        }
+        System.out.println(value);
+    }
+}
