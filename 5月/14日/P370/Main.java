d3ae4826be03503c19c7ff637a546a295a9e65fe
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-14 21:37:46 +0900

    5/14

---
+++ 5月/14日/P370/Main.java
@@ -0,0 +1,24 @@
+class Parent{
+    private static void methodA(){
+        System.out.println("static Parent methodA()");
+    }
+    private void methodB(){
+        System.out.println("Parent#methodB()");
+    }
+
+}
+class Child extends Parent{
+    public static void methodA(){
+        System.out.println("static Child methodA()");
+    }
+    public void methodB(){
+        System.out.println("Child#methodB()");
+    }
+}
+public class Main{
+    public static void main(String[]args){
+        Parent obj = new Child();
+        Child.methodA();
+        obj.methodB();
+    }
+}
