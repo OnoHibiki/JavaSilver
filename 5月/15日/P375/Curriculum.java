e1234fcaff82d3e8b5268c37a816abf13fa44e85
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-15 23:03:42 +0900

    5/15

---
+++ 5月/15日/P375/Curriculum.java
@@ -0,0 +1,11 @@
+public record Curriculum(String name,String level){}
+class Main{
+    public static void main(String...args){
+        Curriculum obj1 = new Curriculum("English", "intermediate");
+        Curriculum obj2 = new Curriculum("Programming", "Advance");
+        obj1.name = "Japanese";//暗黙的にfinalなのでここでエラー
+        System.out.println(obj1);
+        String val = obj2.name() + "," + obj2.level();
+        System.out.println("obj2:" + val);
+    }
+}
