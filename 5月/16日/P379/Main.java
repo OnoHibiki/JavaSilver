f01f931ba6d230d517fb79632439ac22591fa501
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-16 20:13:55 +0900

    5/16

--- 5月/16日/P379/Main.java
+++ 5月/16日/P379/Main.java
@@ -1,7 +1,7 @@
 interface X{}
 class Zero{}
 class One extends Zero{}
-class Two extends Zero implements X {}
+class Two extends Zero implements X {}//TwoはZeroとXを含んでいる
 class Main{
     public static void main(String[]args){
         new Main().test(new Two());
