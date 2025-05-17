847b14a3ccb2154b5da454b492955d613813493c
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-17 19:24:55 +0900

    5/17

---
+++ 5月/17日/P382_1/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+
+record Item(int no,String name){}
+public class Main{
+    public static void main(String[]args){
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item(1,"Laptop"));
+        items.add(new Item(2,"Mobile"));
+        items.add(new Item(2,"Phone"));
+        for(Item i : items){
+            System.out.print(i.no() + ", ");
+        }
+        System.out.println(items.set(1, new Item(3,"Smartwatch")));
+    }
+}
