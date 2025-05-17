16e1f17fe719cf2bfa91a36a2fc34f5d35ae14ca
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-17 19:47:07 +0900

    5/17

--- 5月/17日/P383/Main.java
+++ 5月/17日/P383/Main.java
@@ -3,9 +3,12 @@ import java.util.Map;
 public class Main{
     public static void main(String[]args){
         Map<String, String> map = new HashMap<>();
-        map.add("UK","United Kingdom");//addメソッドはない。putのみ。
-        map.add("DE","Germany");//addメソッドはない。putのみ。
-        map.add("FR","France");//addメソッドはない。putのみ。
+        //map.add("UK","United Kingdom");//addメソッドはない。putのみ。
+        //map.add("DE","Germany");//addメソッドはない。putのみ。
+        //map.add("FR","France");//addメソッドはない。putのみ。
+        map.put("UK","United Kingdom");
+        map.put("DE","Germany");
+        map.put("FR","France");
         System.out.println(map);
     }
 }
