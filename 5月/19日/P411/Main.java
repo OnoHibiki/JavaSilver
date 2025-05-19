6d7b593a7c7b12dd614c9f33f455f9765b46b350
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-19 22:00:57 +0900

    5/19

--- 5月/19日/P411/Main.java
+++ 5月/19日/P411/Main.java
@@ -4,10 +4,13 @@ public class Main{
             String s = null;
             int i1 = s.length();
             int i2 = Integer.parseInt(s);
-      //  } catch(NumberFormatException e){
-      //      System.out.println(e.getMessage());
+        } catch(NumberFormatException e){
+            System.out.println(e.getMessage());
         } catch (NullPointerException e){
             e.printStackTrace();
         }
     }
 }
+
+//catchしないと異常終了。catchしていると、正常終了
+//出てくるエラーは同じでも、終わり方が違う
