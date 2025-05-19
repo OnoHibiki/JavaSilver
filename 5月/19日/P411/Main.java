bc8da11a971c330c4aa76969c50c3f3d9ae35deb
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-19 21:53:03 +0900

    5/19

---
+++ 5月/19日/P411/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[]args){
+        try{
+            String s = null;
+            int i1 = s.length();
+            int i2 = Integer.parseInt(s);
+        } catch(NumberFormatException e){
+            System.out.println(e.getMessage());
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+}
