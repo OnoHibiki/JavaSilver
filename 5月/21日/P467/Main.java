129c10a7e716eac3cb847517393fa1eda9c550e1
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-21 22:34:06 +0900

    5/21

--- 5月/21日/P467/Main.java
+++ 5月/21日/P467/Main.java
@@ -2,7 +2,9 @@ public class Main{
     void method(){
         var x = "Java SE 17";
         var y = new StringBuilder("Java").append("17");
-        var v = y.substring(8); //Java17は6文字なので、ここで例外発生。
+        //var v = y.substring(8); //Java17は6文字なので、ここで例外発生。
+        var v = y.substring(5);
+        
         var z = x.substring(4,6);
         int i = x.equals(y) ? 1 : v.equals(z) ? 2 : 0;
         System.out.println(i);
