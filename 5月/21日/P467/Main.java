4ec36aa385d6d65956d8dc1cd837db002c7cf81c
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-21 22:31:52 +0900

    5/21

---
+++ 5月/21日/P467/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    void method(){
+        var x = "Java SE 17";
+        var y = new StringBuilder("Java").append("17");
+        var v = y.substring(8); //Java17は6文字なので、ここで例外発生。
+        var z = x.substring(4,6);
+        int i = x.equals(y) ? 1 : v.equals(z) ? 2 : 0;
+        System.out.println(i);
+    }
+
+    public static void main(String[]args){
+        //this.method();//staticメソッド内ではthisは使えない。インスタンス参照を通してアクセする必要がある
+        new Main().method();
+    }
+}
