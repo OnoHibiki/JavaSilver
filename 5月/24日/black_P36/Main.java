578e526a6c05145f457d4d628d82aaab7e8c3e64
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-24 15:46:08 +1000

    5/24inAus

--- 5月/24日/black_P36/Main.java
+++ 5月/24日/black_P36/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[]args){
-        String str = "hoge,world";//不変
+        static String str = "hoge,world";//不変
         hello(str);
         System.out.println(str);
     }
