fc5bda1c556dae8c1aa7e6b59abe7744ba435a4a
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-24 15:41:58 +1000

    5/24inAus

--- 5月/24日/black_P36/Main.java
+++ 5月/24日/black_P36/Main.java
@@ -5,8 +5,9 @@ public class Main{
         System.out.println(str);
     }
     private static void hello(String msg){//voidです
-        msg = msg.replaceAll("hoge", "hello");
-        //System.out.println(str);
-        System.out.println(msg);
+        msg.replaceAll("hoge", "hello");
+        System.out.println(str);
+        System.out.println(msg);//replaceAllは置換結果を返すため、出力に反映させるには
+        System.out.println(msg = msg.replaceAll("hoge", "hello"));//変数うへの代入が必要
     }
 }
