3362e9b5ac4481372b33c7fff541c6852c786b73
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-24 15:49:06 +1000

    5/24inAus

--- 5月/24日/black_P36/Main.java
+++ 5月/24日/black_P36/Main.java
@@ -1,12 +1,13 @@
 public class Main{
+    static String str = "これは、static変数を代入したstr";
     public static void main(String[]args){
-        static String str = "hoge,world";//不変
+        String str = "hoge,world";//不変
         hello(str);
-        System.out.println(str);
+        System.out.println(str);//これは、ローカル変数のstr
     }
     private static void hello(String msg){//voidです
         msg.replaceAll("hoge", "hello");
-        System.out.println(str);
+        System.out.println(str);//ここからアクセスできるのは、static変数のstr
         System.out.println(msg);//replaceAllは置換結果を返すため、出力に反映させるには
         System.out.println(msg = msg.replaceAll("hoge", "hello"));//変数うへの代入が必要
     }
