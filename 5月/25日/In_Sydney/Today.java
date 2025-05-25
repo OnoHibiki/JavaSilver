c141781ee4fa2edc140ea096a34892481eaab24d
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-25 23:16:52 +1000

    5/25

--- 5月/25日/In_Sydney/Today.java
+++ 5月/25日/In_Sydney/Today.java
@@ -1,7 +1,8 @@
 public class Today{
     public static void main(String[]args){
         String country = "Sydney";
-
+        String when = "今日";
+        String who = "私は";
         System.out.println("私は今日" + country + "にいます");
     }
 }
