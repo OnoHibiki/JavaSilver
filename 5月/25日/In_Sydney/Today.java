625ed8249433178c2e7f6e81e2ff01633b14d147
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-25 23:22:21 +1000

    5/25

--- 5月/25日/In_Sydney/Today.java
+++ 5月/25日/In_Sydney/Today.java
@@ -3,6 +3,23 @@ public class Today{
         String country = "Sydney";
         String when = "今日";
         String who = "私は";
-        System.out.println("私は今日" + country + "にいます");
+        System.out.println(who + when  + country + "にいます");
+
+        human human = new human("HibikiOno",30,"Japan");
+
+    }
+
+    class human{
+        String name;
+        int age;
+        String country;
+
+        human(String name,int age,String country){
+            this.name = name;
+            this.age = age;
+            this.country = country;
+
+            System.out.println(this.name + ":" + this.age + "歳" + this.country + "出身");
+        }
     }
 }
