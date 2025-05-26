358f573c09c6f48a5446317a92e56dccc85753f7
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-26 18:01:46 +1000

    5/26In Sydney

---
+++ 5月/26日　/P134_2/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+        int a = 1;
+        for(int b = 2,total = 0; b <= 5; b++){
+            total += a * b;
+        }
+        System.out.println(total);//スコープ外
+    }
+}
