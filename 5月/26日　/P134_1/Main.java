8bf50f47968da2c5088955926dfdc2c969ed94f4
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-26 17:52:46 +1000

    5/26In Sydney

---
+++ 5月/26日　/P134_1/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+        for(int i = 1,long j = 2; i < 5; i++){
+            System.out.println(i * j);
+        }
+    }
+}
+
+//forの条件式内での変数宣言は、型が同じでないといけない
