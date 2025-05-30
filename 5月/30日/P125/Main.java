5ec8956933c628348cc68e2d434274a0c36d47ae
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-30 19:51:54 +0900

    5/30

--- 5月/30日/P125/Main.java
+++ 5月/30日/P125/Main.java
@@ -1,7 +1,8 @@
 public class Main{
     public static void main(String[]args){
-        if(false)
-        System.out.println("A");
+        if(false)//この処理が行われることは、未来永劫ない。
+        System.out.println("A");//{}がない時、ifの範囲は１行下まで。
         System.out.println("B");
     }
 }
+
