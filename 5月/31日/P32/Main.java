26584a4c285de223229a390de3695a379db62462
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-31 19:42:13 +0900

    5/31

---
+++ 5月/31日/P32/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[]args){
+        int val = 7;
+        bool flg = true; //こんなプリミティブはない。
+        if (flg == true){
+            do{
+                System.out.println(val);
+            }while(val > 10);
+        }
+
+    }
+}
