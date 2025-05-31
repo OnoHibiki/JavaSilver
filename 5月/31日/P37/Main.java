af10b9a4997a453bf3b023f573ee88bad272e5e3
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-05-31 19:48:11 +0900

    5/31

--- 5月/31日/P37/Main.java
+++ 5月/31日/P37/Main.java
@@ -5,6 +5,8 @@ public class Main{
         System.out.println(str);
     }
     private static void hello(String msg){
-        msg.replaceAll("hoge", "hello");//元のstrを変更しているわけではない。
+        msg.replaceAll("hoge", "hello");//元のstrを変更しているわけではない。しかも、返すだけなのでmsgの値は結局変わっていない。
+        System.out.println(msg);//しかも、返すだけなのでmsgの値は結局変わっていない。
+        System.out.println(msg.replaceAll("hoge", "hello"));//こうすれば置き換えた値を出力できる。
     }
 }
