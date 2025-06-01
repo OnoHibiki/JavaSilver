8d19e4ba99b057fc4b6527ea9cf4c238a1a099bb
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-01 10:24:10 +0900

    6/1

--- 6月/1日/P37_1/Main.java
+++ 6月/1日/P37_1/Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String[]args){
         String str = "abcde";
-        System.out.println(str.charAt(5));//範囲外アクセス
+       //System.out.println(str.charAt(5));//範囲外アクセス
+       System.out.println(str.charAt(4));
     }
 }
