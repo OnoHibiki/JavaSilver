1c64011e6429994783c0dacba3e3b184e9beca63
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-06 22:58:12 +0900

    6/6

--- 6月/6日/P47/Main.java
+++ 6月/6日/P47/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[]args){
-        Item[] item = new Item(3); //Itemクラスにintを引数に取るコンストラクタはないためエラーになる。
+        Item[] item = new Item(3); //Itemクラスにintを引数に取るコンストラクタはないためエラーになる。し、lengthは配列に使う。
         int total = 0 ;
         for(int i = 0 ; i < item.length; i++){
             total += item[i].price;
