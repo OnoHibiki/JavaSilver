926f7528edda3a9ad481194f9ccd33f7cbb4c78a
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-08 10:59:06 +0900

    6/8

--- 6月/8日/P49/Main.java
+++ 6月/8日/P49/Main.java
@@ -3,7 +3,7 @@ public class Main{
         String[][] array = {{"A","B"},null,{"C","D","E"}};
         int total = 0;
         for(String[]tmp : array){
-            total += tmp.length;
+            total += tmp.length; //nullをlengthに渡すことは不可能
         }
         System.out.println(total);
     }
