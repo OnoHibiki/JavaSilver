e7ef73bbbc9a5e26c198c7be1974eb395790137b
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-09 21:36:38 +0900

    6/9

--- 6月/9日/P52/Main.java
+++ 6月/9日/P52/Main.java
@@ -4,11 +4,11 @@ public class Main{
     public static void main(String[]args){
         ArrayList<String> list = new ArrayList<>();
         list.add("A");
-        list.add(2,"B"); //ここのせいで実行時例外
+        list.add(2,"B"); //ここのせいで実行時例外。ここで実行時例外
         list.add("C");
         list.add("D");
         for (String str : list){
-            System.out.print(str);//実際にエラーとして出るのはここ
+            System.out.print(str);
         }
     }
 }
