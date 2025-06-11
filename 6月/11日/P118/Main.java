b639cefbd35502a19fd3da9de232d04c134a8580
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-11 20:47:15 +0900

    6/11

--- 6月/11日/P118/Main.java
+++ 6月/11日/P118/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[]args){
         int a = 3;
-        int b = a += 5;
+        int b = a += 5;//ここでaの値も変わっている
         System.out.println(a + b);
     }
 }
