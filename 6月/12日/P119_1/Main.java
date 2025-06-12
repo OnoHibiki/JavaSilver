a75a0c7b8e769dba3ff9873cd7db322c9aee873c
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-12 18:53:05 +0900

    6/12

--- 6月/12日/P119_1/Main.java
+++ 6月/12日/P119_1/Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[]args){
         boolean a = true;
         boolean b = true;
-        System.out.println(a <= b);
+        System.out.println(a <= b); //booleanに不等号？
     }
+}
