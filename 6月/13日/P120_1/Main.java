109c1e832b28f8e52ef6df8d6055b560d3acaca4
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-13 21:49:20 +0900

    6/13

--- 6月/13日/P120_1/Main.java
+++ 6月/13日/P120_1/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[]args){
         int a = 100, b = 20, c = 30;
-        System.out.println(a % b * c + a / b);
+        System.out.println(a % b * c + a / b); //0乗算が発生するが、別に例外ではない。
     }
 }
