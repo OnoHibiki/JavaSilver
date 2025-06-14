0090c3f3eabf294244923afad453cdc312364740
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-14 18:40:47 +0900

    6/14

--- 6月/day14/P124/Main.java
+++ 6月/day14/P124/Main.java
@@ -2,6 +2,6 @@ public class Main{
     public static void main(String[]args){
         Object a = new Object();
         Object b = null;
-        System.out.println(a.equals(b));
+        System.out.println(a.equals(b));//オーバーライドしない場合、nullとの比較は一律でfalse
     }
 }
