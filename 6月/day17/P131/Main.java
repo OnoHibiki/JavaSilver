f59287e7cf290b589b8cb6436b0605ada3094065
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-17 22:03:46 +0900

    6/17

---
+++ 6月/day17/P131/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[]args){
+        int a = 3;
+        String result = switch(a){
+            case 1 -> "A";
+            case 2 -> "B";
+            case 3 -> "C";
+            default -> "D";
+        }//そう、ここにセミコロンが必要。
+
+        System.out.println(result);
+    }
+}
