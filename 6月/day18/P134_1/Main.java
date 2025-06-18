ff19565188beed7595bfe70913e3a503ff9992bd
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-18 21:32:26 +0900

    6/18

---
+++ 6月/day18/P134_1/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+        for(int i = 1,long j = 2; i < 5 ; i++){
+            System.out.print(i * j);
+        }
+    }
+}
+
+//for条件の中で、異なるプリティブ変数は同時宣言不可。
