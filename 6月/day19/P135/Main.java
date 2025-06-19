1ec5dc6206e0adf0f95978100bbfb4fb94f28347
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-19 19:53:35 +0900

    6/19

--- 6月/day19/P135/Main.java
+++ 6月/day19/P135/Main.java
@@ -1,8 +1,10 @@
 public class Main{
     public static void main(String[]args){
-        for(int i = 0 , j = 0 ; i < 3 , j < 5 ; i++ ){
+        for(int i=0,j=0;i < 3,j < 5;i++ ){
             System.out.println(i++);
             j += i ;
         }
     }
 }
+
+//条件式を複数指定する場合は、,ではなく&&
