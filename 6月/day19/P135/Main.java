b2a6ea4e66ac0e88efc161c4fb44e36a01b356e7
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-19 19:54:08 +0900

    6/19

--- 6月/day19/P135/Main.java
+++ 6月/day19/P135/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[]args){
-        for(int i=0,j=0;i < 3,j < 5;i++ ){
+        for(int i=0,j=0;i < 3 , j < 5;i++ ){
             System.out.println(i++);
             j += i ;
         }
