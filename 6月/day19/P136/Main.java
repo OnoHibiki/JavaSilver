211e3e1218101ecc45a922277647ec76199c80c5
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-19 19:56:51 +0900

    6/19

---
+++ 6月/day19/P136/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[]args){
+        for(int i =0; i < 3 ; i++ , period()){
+            System.out.print(i);
+        }
+    }
+
+    private static void period(){
+        System.out.print(",");
+    }
+}
+
+
+//おしゃれな処理やね
