726b668ee84cd24ee75aad77cf87b8e58e9fbd95
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-06-22 14:41:14 +0900

    6/22

---
+++ 6月/day22/P141/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[]args){
+        String[] array = {"A","B"};
+        for(String b : array){
+            if("B".equals(b))
+                break;
+            System.out.print(b);
+        }
+    }
+}
