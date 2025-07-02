d67d51cc0717e19642985fa66bfad7934b5e4bf5
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-07-02 20:00:56 +0900

    7/2

---
+++ 7月/day2/P488/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[]args){
+        long a,b,c;
+        a = 1;
+        b = 10L;
+        System.out.println(a + "," + b + "," + "," + c);
+    }
+}
+
+//ローカル変数は初期化が必要。暗黙的な初期化がされるのはメンバ変数のみ
