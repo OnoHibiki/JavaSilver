e61e860e94fc991729e208a5428629d385936638
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-07-03 22:50:08 +0900

    7/3

--- 7月/day3/P426/Main.java
+++ 7月/day3/P426/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     static int num = 23;
     public static void main(String[]args){
-        int num = num ;//ダメです
+        int num = num ;//ダメです　Main.numならOK
         System.out.println(num);
     }
 }
