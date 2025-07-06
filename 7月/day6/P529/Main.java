15cbf9fa7473deac8bf279269540a77482a87f8a
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-07-06 20:03:10 +0900

    7/6

---
+++ 7月/day6/P529/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    int a, b;
+    public Sample(int a, int b){
+        init(a,b);
+    }
+    public void init(int a, int b){
+        this.a = a * b;
+        this.b = b * b;    
+    }
+    public static void main(String[]args){
+        int a = 2,b = 3;
+        Sample s = new Sample(a,b);
+        System.out.println(a + "," + b);
+    }
+}
